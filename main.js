6fa3c2116c4297be47d82dca876e132dd29fde7d
leoleotte <leo_leotte@hotmail.com>
2018-09-01 03:42:34 -0300

    arrows sprites and keyboard input test

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//main.js
 let type = "WebGL"
 if(!PIXI.utils.isWebGLSupported()){
     type = "canvas"
@@ -30,32 +31,60 @@ scaleToWindow(app.renderer.view);
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keypress', keyHandler);
+
+//Variables
+let stageLeftBoundary = 200;
+let stageRightBoundary = 600;
+let block;
 
 //Drawing
 loader
   .add("rsc/blocks.png")
+  .add("rsc/arrows.png")
   .load(setup);
 
-  let block;
 
 //This `setup` function will run when the image has loaded
 function setup() {
 
-  let texture = TextureCache["rsc/blocks.png"];
+  let blocksTexture = TextureCache["rsc/blocks.png"];
+  let leftArrowTexCache = TextureCache["rsc/arrows.png"];
+  let rightArrowTexCache = TextureCache["rsc/arrows.png"];
 
-  //Create a rectangle object that defines the position and
-  //size of the sub-image you want to extract from the texture
-  //(`Rectangle` is an alias for `PIXI.Rectangle`)
-  let rectangle = new Rectangle(0, 0, 145, 90);
+  blocksTexture.frame = new Rectangle(0, 0, 145, 90);
+  block = new Sprite(blocksTexture);
 
-  //Tell the texture to use that rectangular section
-  texture.frame = rectangle;
+  leftArrowTexCache.frame = new Rectangle(0, 0, 100, 100);
+  this.left_arrow = new Sprite(leftArrowTexCache);
+  rightArrowTexCache.frame = new Rectangle(100, 0, 100, 100);
+  this.right_arrow = new Sprite(rightArrowTexCache);
 
-  //Create the sprite from the texture
-  block = new Sprite(texture);
+  //Sprites positions
+  block.x += stageLeftBoundary;
+
+  this.left_arrow.x = 0;
+  this.left_arrow.y = 200;
+  this.right_arrow.x = stageRightBoundary;
+  this.right_arrow.y = 200;
+
+  //Pixi interactivity
+  this.left_arrow.interactive = true;
+  this.left_arrow.buttonMode = true;
+  this.left_arrow.on('pointerdown', onButtonPress({direction: 'left'}));
+
+  this.right_arrow.interactive = true;
+  this.right_arrow.buttonMode = true;
+  this.right_arrow.on('pointerdown', onButtonPress({direction: 'right'}));
 
   //Add the block to the stage
   app.stage.addChild(block);
+  app.stage.addChild(this.left_arrow);
+  app.stage.addChild(this.right_arrow);
+
+  //let message = new Text("Hello Pixi!");
+  //app.stage.addChild(message);
   
   //Render the stage   
   app.renderer.render(app.stage);
@@ -65,12 +94,39 @@ function setup() {
   app.ticker.add(delta => main(delta));
 }
 
-  //Main Loop
-  function main(delta){
-    if(block.y <= 400) {
-      block.y += block.scale.x + block.scale.y;
-    } else {
-      block.scale.set(block.scale.x + 1, block.scale.y+1);
-      block.y -= block.scale.x * 100;
-    }
+//Main Loop
+function main(delta){
+  if(block.y <= 400) {
+    //block.y += block.scale.x + block.scale.y;
+  } else {
+    block.scale.set(block.scale.x + 1, block.scale.y+1);
+    block.y -= block.scale.x * 100;
   }
+}
+
+function keyDownHandler(event) {
+  if(event.keyCode == 39) {
+    block.x +=20;
+  }
+  else if(event.keyCode == 37) {
+    block.x -=20;
+  }
+}
+
+function keyHandler(event) {
+  if(event.keyCode == 40) {
+    block.y +=20;
+  }
+}
+
+function onButtonPress(buttonType) {
+  console.log(buttonType);
+  switch(buttonType.direction) {
+    case 'left':
+      block.x -=20;
+      break;
+    case 'right':
+      block.x +=20;
+      break;
+  }
+}
