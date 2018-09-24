4695429c1d6219f8d93d47d4508a23cd69e5d438
leoleotte <leo_leotte@hotmail.com>
2018-09-24 18:12:35 -0300

    changed movement logic; added collision checks; 2 block pieces added; created barebones line removal logic

---
+++ src/blockPiece.js
@@ -0,0 +1,67 @@
+class BlockPiece {
+  constructor(blockInfo, posX, posY) {
+    this.blockInfo = blockInfo;
+    this.posX = posX;
+    this.posY = posY;
+    this.blockSize = 20;
+    this.currentRotation = 0;
+    this.matrixPlacements = [];
+    this.initPiece();
+  }
+
+  initPiece() {
+    switch(this.blockInfo.type) {
+      case "line":
+        this.matrixPlacements = [
+          [{row:0,col:0}, {row:1,col:0}, {row:2,col:0}, {row:3,col:0}],
+          [{row:3,col:0}, {row:3,col:1}, {row:3,col:2}, {row:3,col:3}],
+          [{row:0,col:0}, {row:0,col:1}, {row:0,col:2}, {row:0,col:3}],
+          [{row:0,col:3}, {row:1,col:3}, {row:2,col:3}, {row:3,col:3}],
+        ];
+        this.color = 0x66CCFF;
+      break;
+      case "square":
+        this.matrixPlacements = [
+          [{row:0,col:1}, {row:0,col:2}, {row:1,col:1}, {row:1,col:2}],
+          [{row:0,col:1}, {row:0,col:2}, {row:1,col:1}, {row:1,col:2}],
+          [{row:0,col:1}, {row:0,col:2}, {row:1,col:1}, {row:1,col:2}],
+          [{row:0,col:1}, {row:0,col:2}, {row:1,col:1}, {row:1,col:2}],
+        ];
+        this.color = 0xFFFF00;
+      break;
+      default:
+        this.matrixPlacements = [
+          [{row:0,col:0}, {row:1,col:0}, {row:2,col:0}, {row:3,col:0}],
+          [{row:3,col:0}, {row:3,col:1}, {row:3,col:2}, {row:3,col:3}],
+          [{row:0,col:0}, {row:0,col:1}, {row:0,col:2}, {row:0,col:3}],
+          [{row:0,col:3}, {row:1,col:3}, {row:2,col:3}, {row:3,col:3}],
+        ];
+        this.color = 0x000000;
+      break;
+    }
+
+    this.rectangles = new Graphics();
+    this.rectangles.lineStyle(2, 0xBBBBBB, 1);
+
+    this.rectangles.beginFill(this.color);
+
+    this.matrixPlacements[this.currentRotation].forEach(block => {
+      this.rectangles.drawRect(
+        block.col * this.blockSize, 
+        block.row * this.blockSize,
+        this.blockSize, this.blockSize); //size
+    });
+    this.rectangles.endFill();
+
+    this.rectangles.x = this.posX * this.blockSize;
+    this.rectangles.y = this.posY * this.blockSize;
+  }
+
+  move(x, y) {
+    this.rectangles.x += x * this.blockSize;
+    this.rectangles.y += y * this.blockSize;
+
+    this.posX += x;
+    this.posY += y;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -9,8 +9,20 @@ let Application = PIXI.Application,
     loader = PIXI.Loader.shared,
     resources = PIXI.Loader.shared.resources,
     TextureCache = PIXI.utils.TextureCache,
+    Graphics = PIXI.Graphics,
     Rectangle = PIXI.Rectangle,
-    Sprite = PIXI.Sprite;
+    Sprite = PIXI.Sprite,
+    Text = PIXI.Text;
+
+let blockTypes = [
+  {type:"line", id:10}, 
+  {type:"square", id:20}, 
+  {type:"T", id:30}, 
+  {type:"S", id:40}, 
+  {type:"Z", id:50}, 
+  {type:"left_L", id:60}, 
+  {type:"right_L", id:70}
+];
 
 //Create a Pixi Application
 let app = new Application({ 
@@ -25,22 +37,28 @@ let app = new Application({
 
 //Keyboard Input Handling
 document.addEventListener('keydown', keyDownHandler);
-document.addEventListener('keypress', keyHandler);
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
 
 //Variables
-let blockFallingSpeed = 1;
-let blockStartPosition = 100;
-let block;
 let left_arrow, right_arrow;
+let blockPiece;
+let blockContainer; //container for all rectangles in the current block piece
+let blocksMatrixSize = {x:10, y:20};
+
+let blocksMatrix = [];
+for (let i = 0; i < blocksMatrixSize.x; i++) {
+  blocksMatrix[i] = [];
+  for (let j = 0; j < blocksMatrixSize.y; j++) {
+    blocksMatrix[i][j] = 0;
+  }
+}
 
 
 //Drawing
 loader
-  .add("rsc/blocks.png")
   .add("rsc/arrows.png")
   .load(setup);
 
@@ -48,12 +66,8 @@ loader
 //This `setup` function will run when the image has loaded
 function setup() {
 
-  let blocksTexture = TextureCache["rsc/blocks.png"];
   let leftArrowTexCache = TextureCache["rsc/arrows.png"];
   let rightArrowTexCache = TextureCache["rsc/arrows.png"];
-
-  blocksTexture.frame = new Rectangle(0, 0, 145, 90);
-  block = new Sprite(blocksTexture);
 
   leftArrowTexCache.frame = new Rectangle(0, 0, 100, 100);
   this.left_arrow = new Sprite(leftArrowTexCache);
@@ -69,20 +83,17 @@ function setup() {
   this.right_arrow.on('pointerdown', onButtonPressRight);
 
   //Sprites positions
-  block.x = blockStartPosition;
   this.left_arrow.x = 0;
   this.left_arrow.y = 500;
   this.right_arrow.x = 200;
   this.right_arrow.y = 500;
 
 
-  //Add the block to the stage
-  app.stage.addChild(block);
+  //Add the buttons to the stage
   app.stage.addChild(this.left_arrow);
   app.stage.addChild(this.right_arrow);
 
-  //let message = new Text("Teletris");
-  //app.stage.addChild(message);
+  createBlockPiece();
   
   //Render the stage   
   app.renderer.render(app.stage);
@@ -92,36 +103,147 @@ function setup() {
   app.ticker.add(delta => main(delta));
 }
 
+let autoMoveTime = 0;
+let autoMoveTimeLimit = 60;
 //Main Loop
 function main(delta){
-  if(block.y <= 400) {
-    block.y += blockFallingSpeed;
+  autoMoveTime += delta;
+  if (autoMoveTime >= autoMoveTimeLimit) {
+    movePiece(0, 1);
+    autoMoveTime = 0;
   }
 }
 
+function createBlockPiece() {
+  if (blockPiece) {
+    solidifyBlocksInsideMatrix();
+    app.stage.removeChild(blockPiece.rectangles);
+    clearLines();
+  }
+  blockPiece = new BlockPiece(blockTypes[randomInt(0,1)], 4, 0);
+  app.stage.addChild(blockPiece.rectangles);
+
+  if (blockPiece && checkPieceCollision(0, 0)) {
+    console.log("GAME OVER");
+    //let message = new Text("GAME OVER");
+    //app.stage.addChild(message);
+    return false;
+  }
+}
+
+//////Input
 function keyDownHandler(event) {
   if(event.keyCode == 39) {
-    block.x +=20;
+    onButtonPressRight();
   }
   else if(event.keyCode == 37) {
-    block.x -=20;
-  }
-}
-
-function keyHandler(event) {
-  if(event.keyCode == 40) {
-    block.y +=20;
+    onButtonPressLeft();
+  } 
+  else if(event.keyCode == 40) {
+    onButtonPressDown();
   }
 }
 
 function onButtonPressLeft() {
-  block.x -=20;
+  movePiece(-1, 0);
 }
 
 function onButtonPressRight() {
-  block.x +=20;
+  movePiece(1, 0);
 }
 
-function debugHandler(){
-  console.log("Loucura amigo");
+function onButtonPressDown() {
+  dropPiece();
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setMatrixPieceBlocks(value) {
+  blockPiece.matrixPlacements[blockPiece.currentRotation].forEach(block => {
+    blocksMatrix[blockPiece.posX + block.col][blockPiece.posY + block.row] = value;
+  });
+}
+
+function checkPieceCollision(offsetX, offsetY) {
+  let collision = false;
+  let posx = blockPiece.posX + offsetX;
+  let posy = blockPiece.posY + offsetY;
+
+  //checks for collision with other blocks and stage boundaries
+  blockPiece.matrixPlacements[blockPiece.currentRotation].forEach(block => {
+      if (posx + block.col >= blocksMatrixSize.x || block.col + posx < 0 ||
+          posy + block.row > blocksMatrixSize.y) {
+        collision = true;
+      } else if (blocksMatrix[posx + block.col][posy + block.row]){
+        if (blocksMatrix[posx + block.col][posy + block.row]) {
+          collision = true;
+        }
+      }
+  });
+  return collision;
+}
+
+function movePiece(posX, posY) {
+  let canMove = true;
+
+  if (checkPieceCollision(posX, posY)) {
+    canMove = false;
+  }
+
+  if (canMove) { //clear old positions and fill matrix witch current piece blocks
+    blockPiece.move(posX, posY);
+  } else if (posY > 0) {
+    dropPiece();
+  }
+
+  return canMove;
+}
+
+function dropPiece() {
+  while(!checkPieceCollision(0, 1)) {
+    movePiece(0, 1);
+  }
+  setMatrixPieceBlocks(blockPiece.blockInfo.id);
+  createBlockPiece();
+}
+
+//creates sprites for each block of the piece inside the matrix
+function solidifyBlocksInsideMatrix() {
+  blockPiece.matrixPlacements[blockPiece.currentRotation].forEach(block => {
+    let blockSprite = new Graphics();
+    blockSprite.lineStyle(2, 0xBBBBBB, 1);
+    blockSprite.beginFill(blockPiece.color);
+
+    blockSprite.drawRect(
+      block.col * blockPiece.blockSize, 
+      block.row * blockPiece.blockSize,
+      blockPiece.blockSize, blockPiece.blockSize); //size
+
+    blockSprite.endFill();
+    blockSprite.x = blockPiece.posX * blockPiece.blockSize;
+    blockSprite.y = blockPiece.posY * blockPiece.blockSize;
+    app.stage.addChild(blockSprite);
+    blocksMatrix[blockPiece.posX + block.col][blockPiece.posY + block.row] = blockSprite;
+  });
+}
+
+function clearLines() {
+  let blocksInLine = 0;
+  for(let i = 0; i < blocksMatrixSize.y; i ++) {
+    for (let j = 0; j < blocksMatrixSize.x; j ++) {
+      if (blocksMatrix[j][i]) {
+        blocksInLine ++;
+      }      
+    }
+    if (blocksInLine >= blocksMatrixSize.x - 1) {
+      console.log(blocksInLine);
+      for (let collumn = 0; collumn < blocksMatrixSize.x; collumn ++) {
+        app.stage.removeChild(blocksMatrix[collumn][i]);
+        blocksMatrix[collumn][i] = undefined;
+      }
+    }
+    blocksInLine = 0;
+  }
 }
